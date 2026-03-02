6b988386ed81d5179e24985d1f2cb9568139e88b
muhammed-dev0 <m.bingol.dev@gmail.com>
2026-03-02 14:27:33 +0300

    Dosyalar Yüklendi

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let ikinciDeger = null;
 let islem = null;
 let sonuc = null;
 
-const parent = document.querySelector(".container")
-const input = document.querySelector("#input")
+const parent = document.querySelector(".container");
+const input = document.querySelector("#input");
 
 parent.addEventListener("click", function (event) {
-    let buttons = event.target.closest('button')
-    let buttonTip = buttons.dataset.type
+
+    let buttons = event.target.closest('button');
+    let buttonTip = buttons.dataset.type;
     if (!buttons) return;
     else if (buttonTip === "clear") {
         input.value = ""
@@ -19,14 +20,14 @@ parent.addEventListener("click", function (event) {
     } else if (buttonTip === "number") {
         input.value += buttons.innerText;
     } else if(buttonTip === "operator") {
-        input.value += buttons.innerText
-        islem = buttons.innerText
+        input.value += buttons.innerText;
+        islem = buttons.innerText;
     } else if (buttonTip === "equals") {
         let tamMetin = input.value;
-        let parcala = tamMetin.split(islem)
-        ilkDeger = parseFloat(parcala[0])
-        ikinciDeger = parseFloat(parcala[1])
-        calculate()
+        let parcala = tamMetin.split(islem);
+        ilkDeger = parseFloat(parcala[0]);
+        ikinciDeger = parseFloat(parcala[1]);
+        calculate();
     } 
 
     function calculate() {
@@ -37,12 +38,33 @@ parent.addEventListener("click", function (event) {
         } else if (islem === "x") {
             sonuc = ilkDeger * ikinciDeger;
         } else if (islem === "÷" ) {
-            sonuc = ilkDeger / ikinciDeger
+            if (ikinciDeger === 0) {
+                sonuc = "0'a bölünemez";
+                setTimeout(ekraniTemizle, 1500)
+            }else {
+                sonuc = ilkDeger / ikinciDeger;
+            }
         } else if (islem === "%") {
-            sonuc = ilkDeger / 100
+            sonuc = ilkDeger / 100;
         }
 
-        input.value = sonuc
+        function ekraniTemizle() {
+            input.value = ""
+        }
+
+        if (sonuc === "0'a bölünemez") {
+            input.value = sonuc;
+        } else if (isNaN(sonuc)) {
+            input.value = "Geçersiz İşlem";
+        } else {
+            input.value = sonuc;
+        }
+
+        if (input.value === "Geçersiz İşlem" || input.value === "0'a bölünemez") {
+            setTimeout(ekraniTemizle, 2000);
+        }
+
     }
 
 })
+
